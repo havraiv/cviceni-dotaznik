78c15a9a8d986ea99b0cae3e7bf3e4954003452c
Iva Havranová <havranova.i@seznam.cz>
2023-05-22 19:37:47 +0200

    podmínka

--- src/Question/index.jsx
+++ src/Question/index.jsx
@@ -2,12 +2,15 @@ import React, { useState } from 'react';
 import QuestionBody from '../QuestionBody/index';
 import Option from '../Option/index';
 import './style.css';
-
+// Zařiďte následující: pokud je ve stavu answered hodnota false, chceme, aby componenta QuestionBody měla ikonku symbolQuestion. Pokud je stav true, chceme, aby typ ikonky byl symbolTick.
 const Question = ({ text }) => {
   const [answer, setAnswer] = useState(false);
   return (
     <div className="question">
-      <QuestionBody iconType="symbolQuestion" text={text} />
+      <QuestionBody
+        iconType={answer ? 'symbolTick' : 'symbolQuestion'}
+        text={text}
+      />
       <div className="question__options">
         <Option type="smileyStrongYes" text="Souhlasím" />
         <Option type="smileyYes" text="Spíše souhlasím" />
